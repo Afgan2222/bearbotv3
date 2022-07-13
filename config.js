49b0fc031d03ef189706d959f8b61a018d6c34d0
Afgan2222 <108980042+Afgan2222@users.noreply.github.com>
2022-07-13 20:01:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ global.gopay = '-'
 
 //=============『 Info Bot 』=================//
 global.namebot = 'kanna BOT'
-global.gc = 'https://chat.whatsapp.com/KZ8a8elMR1lHfKoW7kNNto'
+global.gc = 'https://chat.whatsapp.com/DVfawsZozt36QdYmfQlsbD'
 global.web = 'https://xnxx.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
